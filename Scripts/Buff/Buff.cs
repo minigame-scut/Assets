dcc306121156da08705d0255dc781def93afc0b1
Chenxvil <1229029461@qq.com>
2019-06-05 21:01:48 +0800

    Buff and npc animation of map1-0

--- Scripts/Buff/Buff.cs
+++ Scripts/Buff/Buff.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buff : MonoBehaviour
+public enum Buff
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    SUPER,
+    GRAVITY,
+    ELASTIC
 }
