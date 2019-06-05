da8cf3c60d0725e02d85d788497aaaed27779100
ljiabao9 <51065367+ljiabao9@users.noreply.github.com>
2019-06-05 09:19:37 +0800

    Add files via upload

---
+++ Scripts/Object/Face.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //滑稽脸被玩家触碰
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        //玩家的 tag 为 Player
+        if (otherCollider.tag.Equals("Player"))
+        {
+            //获取玩家的控制脚本
+            Player player = otherCollider.gameObject.GetComponent<Player>();
+
+            //更新收集滑稽的个数
+            player.faceCount += 1;
+
+            //销毁关卡滑稽脸自身
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/Object/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(1.5f, 1.5f);
+
+    public bool hasKey = false; //是否拥有当前关卡的钥匙
+    public int faceCount = 0;   //收集的滑稽脸的个数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed.x * inputX, speed.y * inputY);
+    }
+}
