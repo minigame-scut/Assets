d55c67b2baaac83ac624ada96dfcee27dbac54d9
BruceAtian <36923246+BruceAtian@users.noreply.github.com>
2019-06-05 15:01:07 +0800

    更新ScenesManager.cs

--- Scripts/Common/EventType.cs
+++ Scripts/Common/EventType.cs
@@ -1,15 +1,21 @@
 ﻿public enum EventType{
-    ShowText,
-    brokeSpeedDoor,  //玩家触碰到各种门
-    deathDoor,
-    gDoor,
-    inworldDoor,
-    megicalDoor,
-    outWorldDoor,
-    transDoor,
-    upSpeedDoor,
-    birth,   //玩家诞生
-    death,  //玩家死亡
-    nextPlace, //玩家进入下一关
+    SHOWTEXT,
+    //各类门的信号
+    BROKESPEEDDOOR,  //弹力门
+    DEATHDOOR,       //死亡门
+    GDOOR,           //重力门
+    INWORLDDOOR,     //进入里世界的门
+    MAGICALDOOR,     //魔法门
+    OUTWORLDDOOR,    //出去里世界的门
+    TRANSDOOR,       //传送门
+    UPSPEEDDOOR,     //加速门
+    //玩家信号
+    DEATH,           //玩家死亡
+    BIRTH,           //玩家诞生
+    NEXTPLACE,       //玩家进入下一关
+    JUMP,            //玩家跳
+    RUSH,            //玩家冲
+    //道具信号
+    DESTROY          //道具销毁
 
 }
