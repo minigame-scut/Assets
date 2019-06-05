a7d0aadd27102f7516a8499ad6980eb0326b0625
upandown <1370620378@qq.com>
2019-06-05 10:38:43 +0800

    /

--- Scripts/Common/EventType.cs
+++ Scripts/Common/EventType.cs
@@ -1,4 +1,15 @@
 ﻿public enum EventType{
     ShowText,
+    brokeSpeedDoor,  //玩家触碰到各种门
+    deathDoor,
+    gDoor,
+    inworldDoor,
+    megicalDoor,
+    outWorldDoor,
+    transDoor,
+    upSpeedDoor,
+    birth,   //玩家诞生
+    death,  //玩家死亡
+    nextPlace, //玩家进入下一关
 
 }
--- Scripts/Object/transDoorController.cs
+++ Scripts/Object/transDoorController.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//sceneManager负责接收门触碰发出的信号，然后使用该信号来操作玩家做出相应的改变。
-
-public class SceneManager : MonoBehaviour
+public class transDoorController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
