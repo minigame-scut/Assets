fe7087ce32cb6e87513828ef0b687f4700f206f0
Chenxvil <1229029461@qq.com>
2019-06-06 08:56:26 +0800

    switch map animation

---
+++ Scripts/ScenesManager/SwitchMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMap : MonoBehaviour
+{
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+    public void PlayOpenMap()
+    {
+        ani.SetBool("openmap", true);
+    }
+    public void StopOpenMap()
+    {
+        ani.SetBool("openmap", false);
+    }
+    public void PlayCloseMap()
+    {
+        ani.SetBool("closemap", true);
+    }
+    public void StopCloseMap()
+    {
+        ani.SetBool("closemap", false);
+    }
+}
